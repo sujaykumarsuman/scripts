0f695a76a01dacef854c3c4e20abdd25a8566370
Sujay Kumar Suman <sujaykumar@Sujays-MacBook-Pro.local>
2025-08-03 12:42:12 +0530

    add git shorthand for cloning into go projects

--- cdgo.sh
+++ cdgo.sh
@@ -1,7 +1,44 @@
-cdgo() {
+#!/bin/sh
+# cdgo.sh - A script to change directories to specific Go projects
+
+# cdsk sujaykumarsuman
+cdsk() {
   if [ $# -eq 0 ]; then
     echo "Please provide repository name"
     return
   fi
   cd $GOPATH/src/github.com/sujaykumarsuman/$1
 }
+
+_cdsk_completion() {
+  if [ "${#COMP_WORDS[@]}" != "2" ]; then
+    return
+  fi
+
+  projects=$(ls $GOPATH/src/github.com/sujaykumarsuman)
+
+  COMPREPLY=($(compgen -W "${projects}" -- "${COMP_WORDS[1]}"))
+}
+
+complete -F _cdsk_completion cdsk
+
+# skriptvalley
+cdvalley() {
+  if [ $# -eq 0 ]; then
+    echo "Please provide repository name"
+    return
+  fi
+  cd $GOPATH/src/github.com/skriptvalley/$1
+}
+
+_cdvalley_completion() {
+  if [ "${#COMP_WORDS[@]}" != "2" ]; then
+    return
+  fi
+
+  projects=$(ls $GOPATH/src/github.com/skriptvalley)
+
+  COMPREPLY=($(compgen -W "${projects}" -- "${COMP_WORDS[1]}"))
+}
+
+complete -F _cdvalley_completion cdvalley
---
+++ gitshorts.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# gclone takes a repository ssh url and clones it into the $HOME/go/src/<remote>/<repo> directory.
+# This also creates the necessary directories if they do not exist like remote, for ex if github.com does not exist
+# it will create it.
+# Usage: gclone <repository ssh url>
+
+gclone() {
+    if [ $# -eq 0 ]; then
+        echo "Please provide repository ssh url"
+        return
+    fi
+    
+    repo_url=$1
+    remote=$(echo $repo_url | cut -d':' -f1)
+    repo=$(echo $repo_url | cut -d':' -f2-)
+    
+    # Create the necessary directories if they do not exist
+    mkdir -p $HOME/go/src/$remote
+    
+    # Clone the repository
+    git clone $repo_url $HOME/go/src/$remote/$repo
+    echo "Cloned $repo into $HOME/go/src/$remote/$repo"
+    cd $HOME/go/src/$remote/$repo
+}
