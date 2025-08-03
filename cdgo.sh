34010f17c5c9a0d3ad219bf72dd0ae2f7331c074
Sujay Kumar Suman <sujaykumar.dev@gmail.com>
2025-08-03 13:16:12 +0530

    remove .git suffix while creating project dir in gclone

--- cdgo.sh
+++ cdgo.sh
@@ -3,42 +3,49 @@
 
 # cdsk sujaykumarsuman
 cdsk() {
-  if [ $# -eq 0 ]; then
-    echo "Please provide repository name"
-    return
-  fi
-  cd $GOPATH/src/github.com/sujaykumarsuman/$1
+  cd "$GOPATH/src/github.com/sujaykumarsuman/$1"
 }
 
 _cdsk_completion() {
+  local projects
+  projects=($(command ls "$GOPATH/src/github.com/sujaykumarsuman" 2>/dev/null))
+  compadd -- "${projects[@]}"
+}
+
+_cdsk_bash_completion() {
   if [ "${#COMP_WORDS[@]}" != "2" ]; then
     return
   fi
-
-  projects=$(ls $GOPATH/src/github.com/sujaykumarsuman)
-
+  local projects
+  projects=$(command ls "$GOPATH/src/github.com/sujaykumarsuman" 2>/dev/null)
   COMPREPLY=($(compgen -W "${projects}" -- "${COMP_WORDS[1]}"))
 }
-
-complete -F _cdsk_completion cdsk
 
 # skriptvalley
 cdvalley() {
-  if [ $# -eq 0 ]; then
-    echo "Please provide repository name"
-    return
-  fi
-  cd $GOPATH/src/github.com/skriptvalley/$1
+  cd "$GOPATH/src/github.com/skriptvalley/$1"
 }
 
 _cdvalley_completion() {
+  local projects
+  projects=($(command ls "$GOPATH/src/github.com/skriptvalley" 2>/dev/null))
+  compadd -- "${projects[@]}"
+}
+
+_cdvalley_bash_completion() {
   if [ "${#COMP_WORDS[@]}" != "2" ]; then
     return
   fi
-
-  projects=$(ls $GOPATH/src/github.com/skriptvalley)
-
+  local projects
+  projects=$(command ls "$GOPATH/src/github.com/skriptvalley" 2>/dev/null)
   COMPREPLY=($(compgen -W "${projects}" -- "${COMP_WORDS[1]}"))
 }
 
-complete -F _cdvalley_completion cdvalley
+# Register completions
+if [[ -n $ZSH_VERSION ]]; then
+  compdef _cdsk_completion cdsk
+  compdef _cdvalley_completion cdvalley
+else
+  complete -F _cdsk_bash_completion cdsk
+  complete -F _cdvalley_bash_completion cdvalley
+fi
